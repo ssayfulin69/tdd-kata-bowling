1776cd3765f451c5fe99087c5419c9d333723e20
Sergey Sayfulin <sosayfulin@avito.ru>
2020-11-26 17:55:48 +0300

    Initial Commit

---
+++ TddKata_BowlingGame/TddKata_BowlingGame.h
@@ -0,0 +1,18 @@
+//
+//  TddKata_BowlingGame.h
+//  TddKata_BowlingGame
+//
+//  Created by Сайфулин Сергей on 26.11.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TddKata_BowlingGame.
+FOUNDATION_EXPORT double TddKata_BowlingGameVersionNumber;
+
+//! Project version string for TddKata_BowlingGame.
+FOUNDATION_EXPORT const unsigned char TddKata_BowlingGameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TddKata_BowlingGame/PublicHeader.h>
+
+
